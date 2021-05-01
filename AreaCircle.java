cc9e404227517a90a6a8d3f92341309454a90cec
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-01 10:30:02 +0530

    Program to Find Area of Circle

---
+++ AreaCircle.java
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////
+//Function Name:AreaCircle
+//Input:9
+//Output:264.34
+//Description: Program to Find Area of Circle.
+//Date: 01/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+// Program to Find Area of Circle
+
+class AreaCircle
+{  
+	public static final float PI=3.14f; 
+
+	public static void main(String[] args) 
+	{
+		
+		 int r=9
+        //final float PI=3.14f; 
+		 System.out.println("Area of Circle is:"+PI*r*r);
+	}
+}
