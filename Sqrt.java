1a5916a0171c02b325c391b43e9e6e13abd03254
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-01 10:23:56 +0530

    Program to Find Square-root of number.

---
+++ Sqrt.java
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////
+//Function Name:main
+//Input:2 3
+//Output:8
+//Description: Program to Find Square-root of number.
+//Date: 01/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+class Sqrt
+{
+	public static void main(String[] args) 
+	{
+		double i=550.0f;
+		double f=Math.sqrt(i);
+
+		System.out.println(f);
+	}
+}
