8967b860754d9ac2e4dca60302b039e2c502c070
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-01 10:20:37 +0530

    Program to Find Power of number.

---
+++ Pow.java
@@ -0,0 +1,18 @@
+// Program to Find Square-root of number.
+import static java.lang.Math.*;
+
+class Pow
+{
+	public static void main(String[] args) 
+	{
+	    double a=2;
+	    double b=3;
+	    double c=Math.pow(a,b);
+	    double d=PI;
+
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(d);
+
+	}
+}
