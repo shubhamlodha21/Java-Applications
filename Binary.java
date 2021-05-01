39b6496ee55528b8ecea653862dc1cd5573ffe94
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-01 10:19:57 +0530

    Program to Converted Binary Number into Decimal Number.

---
+++ Binary.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////
+//Function Name:main
+//Input:0b1011
+//Output:11
+//Description: Program to Converted Binary Number into Decimal Number.
+//Date: 01/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+
+class Binary
+{
+	public static void main(String[] args) 
+	{
+		int i= 0b1011;
+		System.out.println(i);
+	}
+}
