ffce77dd99a1fcd3dec60b966b256f3f062df819
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-01 10:22:20 +0530

    Program to print Jay Ganesh..!

---
+++ JayGanesh.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////
+//Function Name:main
+//Input:-
+//Output:Jay Ganesh..!
+//Description: Program to print Jay Ganesh..!
+//Date: 01/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+class JayGanesh
+{
+	public static void main(String[] args) 
+	{
+		String str="Jay Ganesh..!";
+		System.out.println(str);
+	}
+}
