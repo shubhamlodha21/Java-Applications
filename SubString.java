c1d6f6c924c841911679f99da90a6c708c11027d
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-02 11:48:09 +0530

    Used of SubString method.

---
+++ SubString.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////
+//Function Name:SubString
+//Input:--
+//Output: y Gap!
+//Description:Used of SubString method.
+//Date: 02/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+class SubString
+{
+	public static void main(String[] args) 
+	{
+		String str="Jay Ganesh..!";
+		String st=str.substring(2,5)+"p!";
+	    System.out.println(st);
+	}
+}
