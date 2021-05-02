2e55b5813b4384f6e404ae3d99e742687b9edef8
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-02 11:49:39 +0530

    program to Concanate two String

---
+++ StringCon.java
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////
+//Function Name:StringCon
+//Input:--
+//Output:Jay Ganesh..!from Shubham Lodha
+//Description:program to Concanate two String. 
+//Date: 02/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+class StringCon
+{
+	public static void main(String[] args) 
+	{
+		String str="Jay Ganesh..!";
+		String st="  from Shubham Lodha";
+		String Con= str+st;
+	    System.out.println(Con);
+
+	}
+}
