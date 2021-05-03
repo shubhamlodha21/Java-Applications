9c0f08dcd1c99665cc3bfadc090e41427011c90d
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-04 00:02:45 +0530

    Display Current Time

---
+++ Time.java
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////
+//Function Name:Time
+//Input:--
+//Output:11:44
+//Description: Display Current Time
+//Date: 04/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+
+import java.util.*;
+import java.time.*;
+
+class Time
+{
+	public static void main(String[] args) 
+	{
+		 System.out.printf("%tc",new Date());
+		 System.out.printf("Current time is:  %tr",new Date());
+
+
+	}
+}
