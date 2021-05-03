297d4563adc0471badff3175a90fdbff7fd153ce
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-05-04 00:01:28 +0530

    Used of BigNumber method

---
+++ BigNumber.java
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////
+//Function Name:BigNumber
+//Input:100
+//Output:100
+//Description: Used of BigNumber method
+//Date: 04/05/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+import java.util.*;
+import java.math.*;
+
+class BigNumber
+{
+	public static void main(String[] args)
+	 {
+		BigInteger a=BigInteger.valueof(100);
+		System.out.println(a);
+	}
+}
