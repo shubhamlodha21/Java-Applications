91a16e2850951e9dd2d8db53d3aa600e19f64986
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-09 00:29:38 +0530

    Division of Two Numbers

---
+++ Division.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////
+//Function Name:Divison
+//Input:10 20 
+//Output:0
+//Description:Division of Two Numbers 
+//Date: 09/06/2021
+//Author: Shubham Lodha
+///////////////////////////////////////////////////////
+class Divide
+{
+	public static void main(String arg[])
+	{
+		int ans = 0;
+		int no1 = 10, no2 = 20;
+		ans = no1 / no2;
+		System.out.println("Division is : "+ans);
+	}
+}
