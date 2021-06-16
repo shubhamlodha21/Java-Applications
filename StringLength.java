b07bc92ae1c846f8b66d33b774181d252ce6d3d4
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-06-17 00:38:10 +0530

    Accept String from user and return string length

---
+++ StringLength.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////
+//Function Name:StringLength
+//Input:Shubham L
+//Output:9
+//Description:Accept String from user and return string length
+//Date: 17/06/2021
+//Author: Shubham Lodha
+//////////////////////////////////////////////////////
+
+
+import java.util.*;
+
+class StringLength
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter string");
+		String str = sobj.nextLine();
+		System.out.println("String length is : "+str.length());
+		
+}
+
+
+
+
