73545830f99a1746b9b0b579a27906c0fe8e58d4
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-17 08:54:42 +0530

    Implementation of hash set class in collection interface

---
+++ hashset/src/hashset/bookhash.java
@@ -0,0 +1,28 @@
+////////////////////////////
+/*class:bookhash
+*Input: 103	java	500
+        102	oracle	600
+        101 cpp     800
+        101 cpp     800
+*Output:101	cpp  	800
+        102	oracle	600
+        103	java	500    
+*Description:Implementation of hash set class in collection interface
+*Date:17-08-2021
+*Author Name:Shubham Lodha
+*///////////////////////////////////
+package hashset;
+
+import java.util.Set;
+
+public class bookhash {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bookinfo bi=new bookinfo();
+		  Set<book> hashset =bi.create();
+		  bi.display(hashset);
+
+	}
+
+}
