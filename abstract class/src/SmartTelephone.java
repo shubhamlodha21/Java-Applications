10d325c2554cf5c8447e02f7a5a0f618a5dea607
Shubham Lodha <83054906+shubhamlodha21@users.noreply.github.com>
2021-08-17 09:17:37 +0530

    Add files via upload

---
+++ abstract class/src/SmartTelephone.java
@@ -0,0 +1,40 @@
+////////////////////////////
+/*class: class SmartTelephone extends Telephone
+*Input:--
+*Output:Lifting!!!!!!
+        Ringing!!!!!!
+        Disconnecting...
+        non abstract method
+*Description:Implementation of abstract class
+*Date:17-08-2021
+*Author Name:Shruti Nahar
+*///////////////////////////////////
+public class SmartTelephone extends Telephone {
+	  @Override
+	void ring() {
+		// TODO Auto-generated method stub
+		System.out.println("Ringing!!!!!!");
+	}
+	  @Override
+	void lift() {
+		// TODO Auto-generated method stub
+		System.out.println("Lifting!!!!!!");	
+	}
+	  @Override
+	void disconnect() {
+		// TODO Auto-generated method stub
+		System.out.println("Disconnecting...");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       SmartTelephone s=new SmartTelephone();
+       s.lift();
+       s.ring();
+       s.disconnect();
+       s.i();
+      
+	}
+
+
+}
---
+++ abstract class/src/Telephone.java
@@ -0,0 +1,11 @@
+
+abstract class Telephone {
+	 abstract void ring();
+	 abstract void lift();
+	 abstract void disconnect();
+	 public void i()
+	 {
+		 System.out.println("non abstract method");
+	 }
+}
+
